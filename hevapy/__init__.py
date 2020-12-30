33895e5bbdb89d4530ade1f592721d38af1d25f0
melioristic <itsmohitanand@gmail.com>
2020-12-30 14:27:08 +0100

    added modules except cart_plot

--- hevapy/__init__.py
+++ hevapy/__init__.py
@@ -1,1 +1,7 @@
+from hevapy import details
+from hevapy import dist
+from hevapy import gev
+from hevapy import graphics
 from hevapy import helper
+from hevapy import io
+from hevapy import preprocess
